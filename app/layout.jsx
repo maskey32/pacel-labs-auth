247eabf05094719fb652a8444c278ff12570877e
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2024-01-19 08:30:36 +0100

    chore: Added column

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ import Footer from '@components/Footer';
 
 export const metadata = {
     title: 'Pacel Labs Auth',
-    description: 'Basic login and logout functionality'
+    description: 'Basic login and logout functionality',
 };
 
 const RootLayout = ({ children }) => {
